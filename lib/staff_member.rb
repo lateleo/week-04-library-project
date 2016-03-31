934c87aa99c4bad6b4f9e819d3f2900c2dedf836
Art Burtch <artburtch@huskers.unl.edu>
2016-03-31 18:38:43 -0500

    email validator still not working

--- lib/staff_member.rb
+++ lib/staff_member.rb
@@ -3,8 +3,7 @@ require "active_record"
 class StaffMember < ActiveRecord::Base
 # has two attributes, name and email, only the latter of which is unique.
   validates :name, presence: {message: "name is invalid"}
-  validates :email, uniqueness: true#, format: {with: /\A#{name}\d{1,4}@\w+\.[a-z]{2,3}\Z/, message: "email is invalid"}
-  before_validation :validate_email
+  validates :email, uniqueness: true, format: {with: /\A#{name}\d{1,4}@\w+\.[a-z]{2,3}\Z/, message: "email is invalid"}
 
   def validate_email
     /\A#{name.delete("^a-zA-Z").downcase}\d{1,4}@\w+\.[a-z]{2,3}\Z/ =~ email ? true : false
