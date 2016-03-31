b51f6aa69f2fbfd8e2553f48eb8c7e4ca6fa46e7
Art Burtch <artburtch@huskers.unl.edu>
2016-03-31 18:46:22 -0500

    added book validators and table

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,8 @@
 require "active_record"
 
 class Book < ActiveRecord::Base
-
+# three attributes: title, author and ISBN, the latter of which is unique.
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, uniqueness: true
 end
