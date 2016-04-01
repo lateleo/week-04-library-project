d8ab21e4525fab525dc5e973601d1cd19e53a6e2
Art Burtch <artburtch@huskers.unl.edu>
2016-04-01 18:56:36 -0500

    added main_helper and sub_menu_methods because main.rb was getting cluttered.

--- main.rb
+++ main.rb
@@ -1,16 +1,8 @@
-require_relative "config/environment.rb"
-require "active_record"
-require "pry"
-require "yaml"
-require_relative "./lib/book.rb"
-require_relative "./lib/branch.rb"
-require_relative "./lib/patron.rb"
-require_relative "./lib/staff_member.rb"
+require_relative "main_helper.rb"
 
-def invalid_option_message
-  print "That's not a valid option. Please try again: "
-end
-
+# This method simply lists all the available options in the main menu.
+# While it has a parameter, it doesn't do anything with it, but the parameter
+# has to be there because of how it is called in retrieve_choice.
 def main_options(menu)
   print "---------------------------------------------------------------
   \nMain menu options:\n
@@ -21,6 +13,8 @@ def main_options(menu)
   exit: Exit\n\nWhat would you like to do? "
 end
 
+# This method lists all the available options in the main sub menu
+# of the given name (in this case, either branch, staff member, book or patron).
 def main_sub_menu_options(sub_menu_name)
   print "---------------------------------------------------------------
   \n#{sub_menu_name.capitalize} menu options:\n
@@ -29,7 +23,19 @@ def main_sub_menu_options(sub_menu_name)
   exit: Exit to Main menu\n\nWhat would you like to do? "
 end
 
-def retrieve_choice(menu_type, sub_menu_name)
+#---------------------------------------------------------------------
+
+# A simple method used by retrieve_choice to inform the user that their choice
+# wasn't valid and prompt them for another.
+def invalid_option_message
+  print "That's not a valid option. Please try again: "
+end
+
+# Used by (all?) menu methods to list the available options in the given menu
+# and prompt the user for a choice, and then returns that choice. The second parameter
+# is only used by first_tier_sub_menu, wherein the menu_type is "sub menu", and thus
+# the method needs a name to send to main_sub_menu_options.
+def retrieve_choice(menu_type, sub_menu_name = nil)
   menu_options = {"main" => [:main_options, 4],
                   "sub menu" => [:main_sub_menu_options, 2]}
   send(menu_options[menu_type][0], sub_menu_name)
@@ -37,13 +43,22 @@ def retrieve_choice(menu_type, sub_menu_name)
   choice
 end
 
+#---------------------------------------------------------------------
+
+# The main menu method. continues to run until retrieve_choice returns "exit".
+# On each cycle, passes a string based on the user's choice to first_tier_sub_menu,
+# which indicates which records the user would like to view.
 def main_menu
-  while (choice = retrieve_choice("main", "main")) != "exit"
+  while (choice = retrieve_choice("main")) != "exit"
     sub_menus = ["branch", "staff member", "book", "patron"]
     first_tier_sub_menu(sub_menus[choice.to_i - 1])
   end
 end
 
+# The method used for all four main sub menus. Originally, they were four separate
+# menu methods, but they were virtually identical, so I combined them for ease of
+# use and readability, and then added a string parameter that tells the method which sub menu
+# it should display to the user.
 def first_tier_sub_menu(sub_menu_name)
   while (choice = retrieve_choice("sub menu", sub_menu_name)) != "exit"
     sub_menus = [:browse_all, :create_new_entry]
@@ -51,14 +66,30 @@ def first_tier_sub_menu(sub_menu_name)
   end
 end
 
+#---------------------------------------------------------------------
+
+# Used by first_tier_sub_menu to call the appropriate browsing method based
+# on the sub menu it came from. This includes a unique method for each model
+# because it is beyond this point that the paths are no longer interchangeable,
+# due to the difference in columns between the different models.
 def browse_all(sub_menu_name)
-  puts "---------------------------------------------------------------
-  \n\nSo you'd like to browse all #{sub_menu_name.pluralize}? Right this way!\n\n\n...\n\n\n"
+  browser_methods = {"branch" => :browse_branches
+                    "staff member" => :browse_staff_members
+                    "book" => :browse_books
+                    "patron" => :browse_patrons}
+  send(browser_methods[sub_menu_name])
 end
 
+# Used by first_tier_sub_menu to call the appropriate creation method based
+# on the sub menu it came from. This includes a unique method for each model
+# because it is beyond this point that the paths are no longer interchangeable
+# due to the difference in columns between the different models.
 def create_new_entry(sub_menu_name)
-  puts "---------------------------------------------------------------
-  \n\nSo you'd like to enter a new #{sub_menu_name}? Right this way!\n\n\n...\n\n\n"
+  creation_methods = {"branch" => :create_new_branch
+                    "staff member" => :create_new_staff_member
+                    "book" => :create_new_book
+                    "patron" => :create_new_patron}
+  send(browser_methods[sub_menu_name])
 end
 
 main_menu
---
+++ sub_menu_methods/branch_methods.rb
@@ -0,0 +1,12 @@
+require_relative "../lib/book.rb"
+require_relative "../lib/branch.rb"
+require_relative "../lib/patron.rb"
+require_relative "../lib/staff_member.rb"
+
+def create_new_branch
+  true
+end
+
+def browse_branches
+  true
+end
