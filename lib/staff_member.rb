02e896d54f554ccce2f54cec570b0971219f916c
Art Burtch <artburtch@huskers.unl.edu>
2016-04-01 10:45:02 -0500

    email validator fixed, thanks Andrew

--- lib/staff_member.rb
+++ lib/staff_member.rb
@@ -3,10 +3,13 @@ require "active_record"
 class StaffMember < ActiveRecord::Base
 # has two attributes, name and email, only the latter of which is unique.
   validates :name, presence: {message: "name is invalid"}
-  validates :email, uniqueness: true, format: {with: /\A#{name}\d{1,4}@\w+\.[a-z]{2,3}\Z/, message: "email is invalid"}
+  validates :email, uniqueness: true
+  validate :validate_email
 
   def validate_email
-    /\A#{name.delete("^a-zA-Z").downcase}\d{1,4}@\w+\.[a-z]{2,3}\Z/ =~ email ? true : false
+    unless /\A#{name.downcase.delete("^a-z")}\d{1,4}@\w+(\.[a-z]{2,3}){1,2}\Z/ =~ email
+      errors.add(:email, "invalid email format")
+    end
   end
 
 end
