908175ac916a4066d9a30f59922deb790246f4c6
Art Burtch <artburtch@huskers.unl.edu>
2016-04-12 10:20:05 -0500

    condensed require statements

--- main.rb
+++ main.rb
@@ -3,16 +3,8 @@ require "sinatra/activerecord"
 require "sinatra/reloader"
 require "pry"
 
-require_relative "app/models/work.rb"
-require_relative "app/models/branch.rb"
-require_relative "app/models/patron.rb"
-require_relative "app/models/staff_member.rb"
-require_relative "app/models/copy.rb"
-
-require_relative "app/controllers/branches_controller.rb"
-require_relative "app/controllers/staff_members_controller.rb"
-require_relative "app/controllers/works_controller.rb"
-require_relative "app/controllers/patrons_controller.rb"
+Dir[File.dirname(__FILE__) + '/app/models/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/app/controllers/*.rb'].each {|file| require file }
 
 get "/" do
   @page_name = "Home"
